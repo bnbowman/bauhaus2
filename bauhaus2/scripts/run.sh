27340ee86888193eb326c7b509effc9db75a5c5e
David Alexander <dalexander@pacificbiosciences.com>
2017-02-15 08:35:22 -0800

    By default, keep the R environment hygienic by excluding user libs
    This involves exporting an env var R_IGNORE_USER_LIBS that will now be
    understood by the R/* modules

--- bauhaus2/scripts/run.sh
+++ bauhaus2/scripts/run.sh
@@ -2,6 +2,12 @@
 source /mnt/software/Modules/current/init/bash
 
 module load smrtanalysis/mainline
+
+# The latter line here is a directive to the R module load: don't let
+# user's R environment interfere with our scripts.  Developers may
+# want to comment out these lines.
+unset R_LIBS
+export R_IGNORE_USER_LIBS=1
 module load R/3.2.3-internal
 
 ## FIXME: need to configurify this..
