9d8e841e5bc7f9c76309f383d309baae837fd455
David Alexander <dalexander@pacificbiosciences.com>
2017-03-02 15:21:44 -0800

    Remove "module load bauhaus2" from run.sh
    This was causing grief for developers, who need to use the bauhaus2 library in their virtualenv

--- bauhaus2/scripts/run.sh
+++ bauhaus2/scripts/run.sh
@@ -2,10 +2,5 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $THISDIR
 
-source /mnt/software/Modules/current/init/bash
-module use /mnt/software/modulefiles
-module purge
-module load bauhaus2/master
-
 mkdir -p log conditions reports
 snakemake {{ cluster_options }} -p -s workflow/Snakefile --configfile config.json 2>&1 | tee snakemake.log
