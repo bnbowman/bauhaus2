e5ee0a54825274a812e19677ed473a3dfdde8fc5
Martin D. Smith <mdsmith@pacificbiosciences.com>
2018-01-09 16:06:39 -0800

    Add AccuracyDelta, PrimaryRuntimes figures

--- bauhaus2/workflows/primary.py
+++ bauhaus2/workflows/primary.py
@@ -56,6 +56,7 @@ class PrimaryRefarmWorkflow(Workflow):
                              "Python/GetZiaTags.py")
 
     def plan(self):
-        return (["summarize-mappings.snake", "constant-arrow.snake", "constant-arrow-regular.snake", 
-                 "heatmaps.snake", "locacc.snake", "uid-tag.snake", "primary-refarm.snake"]
+        return (["primary-refarm.snake", "summarize-mappings.snake",
+                 "constant-arrow.snake", "constant-arrow-regular.snake",
+                 "heatmaps.snake", "locacc.snake", "uid-tag.snake"]
                 + subreadsMappingPlan(self.conditionTable, self.cliArgs))
