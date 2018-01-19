81a194c373b043f2e7f6f65ceeb11f1d8d553703
Kristofor Nyquist <knyquist@pacificbiosciences.com>
2018-01-19 11:44:46 -0800

    improved bursts/reads resize function

--- bauhaus2/resources/scripts/Python/CollectPpaBurstMetrics.py
+++ bauhaus2/resources/scripts/Python/CollectPpaBurstMetrics.py
@@ -1,27 +1,54 @@
 """
 Created on or around Jan 17, 2018
 
+This script calls the BurstMetrics library in the
+biochemistry-toolkit, collecting PPA-classified burst
+metrics if they are available. Otherwise (i.e. for
+customer-facing data), this script writes 'SubreadSet
+does not contain PPA burst info' into the output CSVs
+
 @author: knyquist
 """
 
 import argparse
+import pandas
 import csv
+import biotk.libs.BurstMetrics as bm
 
 def parseArgs():
     parser = argparse.ArgumentParser(
-    	description='Generate PPA Burst Metrics CSV')
+        description='Generate PPA Burst Metrics CSV')
     parser.add_argument('subreadset',
-    					help='Path to subreadset XML')
-    parser.add_argument('output',
-    					help='Path to output CSV')
+                        help='Path to subreadset XML')
+    parser.add_argument('output_bursts',
+                        help='Path to bursts output CSV')
+    parser.add_argument('output_reads',
+                        help='Path to reads output CSV')
     args = parser.parse_args()
     return args
 
 def main():
-	args = parseArgs()
-	with open(args.output, 'wb') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow(['hello world!'])
+    args = parseArgs()
+    ppa_bursts = bm.PpaBurstMetrics(args.subreadset,
+                                    subsampleto=10000000)
+
+    # load the reads info into pandas dataframe and save to csv
+    if hasattr(ppa_bursts, 'reads'):
+        reads_summary = pandas.DataFrame.from_records(ppa_bursts.reads)
+        reads_summary.to_csv(args.output_reads)
+    else:
+        with open(args.output_reads, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['SubreadSet does not contain PPA burst info'])
+
+    # load the bursts info into pandas dataframe and save to csv
+    if hasattr(ppa_bursts, 'ppa_bursts'):
+        bursts_summary = pandas.DataFrame.from_records(ppa_bursts.ppa_bursts)
+        bursts_summary.to_csv(args.output_bursts)
+    else:
+        with open(args.output_bursts, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['SubreadSet does not contain PPA burst info'])
 
 if __name__ == '__main__':
     main()
