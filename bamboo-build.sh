a4c22fae40b8a93cf7651cbce2b40701f2a81f17
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2018-04-13 13:56:25 -0700

    adding the first version of bamboo-build.sh

---
+++ bamboo-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+#./ci/buildAndTest.sh
+source /mnt/software/Modules/current/init/bash
+module load python/3.5.1
+module load virtualenv/13.0.1
+
+virtualenv -p python3.5 ./VE
+source ./VE/bin/activate
+
+pip install -U wheel pip
+pip install setuptools==33.1.1
+pip install -r requirements.txt
+pip install -r requirements-test.txt
+pip install snakemake
+python setup.py install
+
+echo General tests:
+cram test/cram/*.t
+echo MappingReport tests:
+cram test/cram/internal/executionTest.t
+echo CCS Mapping:
+cram test/cram/internal/ccsTest.t
+echo Cas9 Yield:
+cram test/cram/internal/cas9YieldTest.t
+echo Unrolled test:
+cram test/cram/internal/unrolledExecutionTest.t
+echo Unrolled by reference test:
+cram test/cram/internal/unrolledMultipleContigsExecutionTest.t
+echo Arrow Training:
+cram test/cram/internal/arrowTrainExecutionTest.t
+#echo HQRF:
+#cram test/cram/internal/hqrfExecutionTest.t
+echo Heatmaps:
+cram test/cram/internal/heatmaps.t
+echo BarcodingQC:
+cram test/cram/internal/barcodingQCTest.t
+echo Coverage Titration:
+cram test/cram/internal/coveragetitrationExecutionTest.t
+#echo Primary Refarm:
+#cram test/cram/internal/primaryExecutionTest.t
+echo IsoSeq:
+cram test/cram/internal/isoseqExecutionTest.t
+echo Constant arrow:
+cram test/cram/internal/constantarrowTest.t
+
+#bauhaus2 --no-smrtlink --noGrid generate -w MappingReports -t test/data/two-tiny-movies.csv -o mapping-reports
+#cd mapping-reports
+#./run.sh
+#cd ..
+
+#make all-tests
