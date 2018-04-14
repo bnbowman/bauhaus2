444d3a3764d233ac4d551822384f0bc3965e0ac7
David Seifert <dseifert@pacificbiosciences.com>
2018-04-14 16:26:09 +0200

    Define enabled tests in Makefile to parallize testsuite

--- bamboo-build.sh
+++ bamboo-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 source /mnt/software/Modules/current/init/bash
@@ -20,27 +20,7 @@ pip install -r requirements-test.txt
 pip install snakemake
 python setup.py install
 
-echo General tests:
-cram test/cram/*.t
-echo MappingReport tests:
-cram test/cram/internal/executionTest.t
-echo CCS Mapping:
-cram test/cram/internal/ccsTest.t
-echo Cas9 Yield:
-cram test/cram/internal/cas9YieldTest.t
-echo Unrolled test:
-cram test/cram/internal/unrolledExecutionTest.t
-echo Unrolled by reference test:
-cram test/cram/internal/unrolledMultipleContigsExecutionTest.t
-echo Arrow Training:
-cram test/cram/internal/arrowTrainExecutionTest.t
-echo Heatmaps:
-cram test/cram/internal/heatmaps.t
-echo BarcodingQC:
-cram test/cram/internal/barcodingQCTest.t
-echo Coverage Titration:
-cram test/cram/internal/coveragetitrationExecutionTest.t
-echo IsoSeq:
-cram test/cram/internal/isoseqExecutionTest.t
-echo Constant arrow:
-cram test/cram/internal/constantarrowTest.t
+echo "#################"
+echo "# Running tests #"
+echo "#################"
+make -j8 all-tests
