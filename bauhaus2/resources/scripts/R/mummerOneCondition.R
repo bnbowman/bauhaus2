25986d1e50c31d87ebd54be613d66c1099a8bc45
Sabrina Xu <sxu@smtp.nanofluidics.com>
2018-04-18 15:29:51 -0700

    Fixing the Plots to Show on Zia

--- bauhaus2/resources/scripts/R/mummerOneCondition.R
+++ bauhaus2/resources/scripts/R/mummerOneCondition.R
@@ -5,11 +5,12 @@ library(argparse)
 parser <- ArgumentParser()
 parser$add_argument("--outDir", nargs = 1, default = "", help = "output directory")
 parser$add_argument("--inDir", nargs = 1, default = "", help = "input directory")
+parser$add_argument("--conditionname", nargs = 1, default = "", help = "condition name")
 try(args <- parser$parse_args())
 
 fileName <- paste(args$inDir,'fastalengthfile.txt', sep = "")
 string1 = readChar(fileName, file.info(fileName)$size)
-pngpath = paste(args$outDir,"myoutput.png", sep="")
+pngpath = paste(args$outDir,args$conditionname,"_myoutput.png", sep="")
 png(pngpath, width=4, height=4, units="in", res=300)
 plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
 text(x = 0.34, y = 0.34, paste(string1), 
