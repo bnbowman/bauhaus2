9fe9ba3b0370ceced590ee136d068699407cf3ab
tian52 <ytian@pacificbiosciences.com>
2018-04-26 15:58:13 -0700

    Combine the assembly csv files across conditions

--- bauhaus2/resources/scripts/Python/convertJsonToCSV.py
+++ bauhaus2/resources/scripts/Python/convertJsonToCSV.py
@@ -7,10 +7,12 @@ from pandas.io.json import json_normalize
 parser = argparse.ArgumentParser(description='Convert Json Report to CSV')
 parser.add_argument('json', help='Input Json File')
 parser.add_argument('csv', help='Output CSV File')
+parser.add_argument('condition', help='Condition Name')
 args = parser.parse_args()
 
 with open(args.json) as f:
      d = json.load(f)
 
 nycphil = json_normalize(d['attributes'])
+nycphil['condition'] = args.condition
 nycphil.to_csv(args.csv)
