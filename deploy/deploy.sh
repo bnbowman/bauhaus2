c3aa1e772b26db278bb575ebd84c5c78cbcde9a1
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2018-04-30 12:35:27 -0700

    use a symbolic link for the switch, don't remove
    We will do an expiration mechanism in the next PR

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,11 +14,13 @@ set -x
 # Set up the virtualenv
 #
 BAUHAUS2_VE=${BAUHAUS2_VE:-/pbi/dept/itg/bauhaus2/python-ve}
+BAUHAUS2_VELINK=$BAUHAUS2_VE
+BAUHAUS2_VE=${BAUHAUS2}_$(/usr/bin/date +%s)
 rm -rf $BAUHAUS2_VE
 
 set +u
 /mnt/software/v/virtualenv/13.0.1/virtualenv.py -p python3 $BAUHAUS2_VE
-source $BAUHAUS2_VE/bin/activate
+source ${BAUHAUS2_VE}/bin/activate
 set -u
 
 pip install -r requirements.txt
@@ -28,5 +30,6 @@ python setup.py install
 #
 # Set up the wrapper scripts
 #
-ln -sf /pbi/dept/itg/bauhaus2/python-ve/bin/bauhaus2 /mnt/software/b/bauhaus2/bauhaus2
-ln -sf /pbi/dept/itg/bauhaus2/python-ve/bin/snakemake /mnt/software/b/bauhaus2/snakemake
+ln -sfn $BAUHAUS2_VE $BAUHAUS2_VELINK
+ln -sf ${BAUHAUS2_VELINK}/bin/bauhaus2 /mnt/software/b/bauhaus2/bauhaus2
+ln -sf ${BAUHAUS2_VELINK}/bin/snakemake /mnt/software/b/bauhaus2/snakemake
