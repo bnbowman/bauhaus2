9dfa2171d555045dfb015e045afbf2857e5cbf1e
tian52 <ytian@pacificbiosciences.com>
2018-05-22 12:06:31 -0700

    Resume snakemake jobs twice for failed bh2 jobs

--- bauhaus2/resources/scripts/run.sh
+++ bauhaus2/resources/scripts/run.sh
@@ -5,4 +5,4 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $THISDIR
 
 mkdir -p log conditions reports
-snakemake {{ cluster_options }} -p -s workflow/Snakefile --configfile config.json 2>&1 | tee snakemake.log
+snakemake {{ cluster_options }} --restart-times 2 -p -s workflow/Snakefile --configfile config.json 2>&1 | tee snakemake.log
